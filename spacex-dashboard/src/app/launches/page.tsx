f58ed44a79b9a43c67f59fc94fc9eea2207459b2
Vitalii-Pavliuk <vitalikpavlyuk67@gmail.com>
2026-01-13 19:48:55 +0200

    Add/pagination

--- spacex-dashboard/src/app/launches/page.tsx
+++ spacex-dashboard/src/app/launches/page.tsx
@@ -2,23 +2,53 @@ import { getLaunches } from "../../lib/api";
 import { LaunchCard } from "../../components/LaunchCard";
 import Link from "next/link";
 
-export default async function Home() {
-  const launches = await getLaunches();
+const ITEMS_PER_PAGE = 20;
 
-  return (
-    <div>
+type Props = {
+  searchParams: Promise<{ page?: string }>; 
+};
+
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
+
+  const allLaunches = await getLaunches();
+
+  const totalPages = Math.ceil(allLaunches.length / ITEMS_PER_PAGE);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentLaunches = allLaunches.slice(startIndex, endIndex);
+
+return (
+  <div>
+      <div>
         <Link href="/">
-        ← Back to Launches
+        ← Back to Rockets
       </Link>
-      <div>
-        <h1>SpaceX Launches</h1>
-        <p>Всього запусків: {launches.length}</p>
       </div>
-      <div>
-        {launches.map((launch) => (
-          <LaunchCard key={launch.id} launch={launch} />
-        ))}
-      </div>
-        </div>
-  );
+  <div>
+    <h1>SpaceX Launches</h1>
+    <div>
+      {currentLaunches.map((launch) => (
+        <LaunchCard key={launch.id} launch={launch} />
+      ))}
+    </div>
+    <div>
+      {currentPage > 1 ? (
+        <Link href={`/?page=${currentPage - 1}`}>
+          ← Prev
+        </Link>
+      ) : <div />}
+      <span>
+        Page {currentPage} of {totalPages}
+      </span>
+      {currentPage < totalPages ? (
+        <Link href={`/launches?page=${currentPage + 1}`}>
+          Next →
+        </Link>
+      ) : <div />}
+    </div>
+  </div>
+  </div>
+);
 }
--- spacex-dashboard/src/app/page.tsx
+++ spacex-dashboard/src/app/page.tsx
@@ -1,15 +1,12 @@
-import { getLaunches } from "../lib/api";
-import { LaunchCard } from "../components/LaunchCard";
 import Link from "next/link";
 
 export default async function Home() {
-  const launches = await getLaunches();
-
   return (
     <main>
         <Link href="/launches">
         Open launcehs list
       </Link>
+      <br />
         <Link href="/rockets">
         Open rockets list
       </Link>
